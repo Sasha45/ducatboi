7f16c4e593b71ca1e83c8b940f49b02a9a7e4222
Sasha Kubichka <s@kubichka.kiev.ua>
2018-08-06 13:34:24 -0700

    change prefix, fix arg, add say, fix leave

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client({});
 const auth=require("./auth.json");
-const prefix = "!";
+const prefix = "$";
 
 bot.on("ready", ()=>{
 	//bot.user.setGame('with buckets');
@@ -12,8 +12,8 @@ bot.on("message", async message => {
 	if(message.content.startsWith(prefix) !== true) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const arg = message.content.substring().trim();
- 	const command = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
+	const arg = message.content.substring(prefix.length+command.length).trim();
 
  	// if(command=="test"){
  	// 	message.channel.send("Test successful. Congratulations, you done did a thing.");
@@ -25,23 +25,26 @@ bot.on("message", async message => {
  		message.channel.send("$21,781.42");
 	}
 	if(command=="ducatsinabucket"){
-		message.channel.send("There are 146.51 ducats in a bucket.")
+		message.channel.send("There are 146.51 ducats in a bucket.");
 	}
- 	if(command=="toDollars"){
- 		let value = arg*148.67;
+ 	if(command=="todollars"){
+ 		let value = parseFloat(arg)*148.67;
  		message.channel.send("$" + value);
  	}
- 	if(command=="shutup" || command=="shutUpPlease"){
+ 	if(command=="shutup" || command=="shutupplease"){
  		message.member.voiceChannel.join().then(connection => {
 			const dispatcher = connection.playFile('hammidshutup.mp3');
 		 });
  		
 	 }
-	if(command=="orca"){
+	// if(command=="orca"){
+	// }
+	if(command=="leave"){
+		message.member.voiceChannel.leave();
 	}
-	 if(command=="leave"){
-		bot.voiceChannel.leave();
-	 }
+	if(command=="say"){
+		message.channel.send(arg);
+	}
 
 
 })
